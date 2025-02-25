2dcce8ef2d37744b41aa610beb3ad082d0171697
R-scan1 <deepr7590@gmail.com>
2025-02-25 16:51:14 +0530

    [Raj] add. using slf4j annotation for logging

--- EmployeePayrollApp/src/main/java/com/bridgelabz/employeepayrollapp/dto/EmployeeDTO.java
+++ EmployeePayrollApp/src/main/java/com/bridgelabz/employeepayrollapp/dto/EmployeeDTO.java
@@ -1,9 +1,14 @@
 package com.bridgelabz.employeepayrollapp.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
-@Data  
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class EmployeeDTO {
     private String name;
     private double salary;
+    
 }
