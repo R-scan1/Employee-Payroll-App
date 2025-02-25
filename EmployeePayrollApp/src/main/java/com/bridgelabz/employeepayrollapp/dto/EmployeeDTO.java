40358058379efb459de2d872331a7e750817acfb
R-scan1 <deepr7590@gmail.com>
2025-02-25 15:00:44 +0530

    [Raj] add. using lambok library for dto

--- EmployeePayrollApp/src/main/java/com/bridgelabz/employeepayrollapp/dto/EmployeeDTO.java
+++ EmployeePayrollApp/src/main/java/com/bridgelabz/employeepayrollapp/dto/EmployeeDTO.java
@@ -1,22 +1,9 @@
 package com.bridgelabz.employeepayrollapp.dto;
 
+import lombok.Data;
+
+@Data  
 public class EmployeeDTO {
     private String name;
     private double salary;
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public double getSalary() {
-        return salary;
-    }
-
-    public void setSalary(double salary) {
-        this.salary = salary;
-    }
 }
